b7538f0ac30514a9a4e3d967b673f49eb3930d92
Mohamed Al Elew <13629454+malelew@users.noreply.github.com>
2026-05-24 18:00:16 -0500

    edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Best week in LNK</h1>
+<h2>May 30 to June 6, 2025</h2>
+<p>It only comes once every few years and it starts now.</p>
